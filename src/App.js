805f98c6ec0fce82e064877681f3080baaa33e37
mij <mijsmatthew@gmail.com>
2023-10-15 17:34:58 -0700

    Add images, button functionality, create css files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+    const [animals, setAnimals] = useState([]);
+
     const handleClick = () => {
-        console.log("button was clicked");
+        //add new random animal to end of array
+        setAnimals([...animals, getRandomAnimal()]);
     }
+
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    });
 
     return (
         <div>
-            <button onClick={handleClick}>Add Animals</button>
+            <button onClick={handleClick}>Add Animal</button>
+            <div>{renderedAnimals}</div>
         </div>
     )
 }   
