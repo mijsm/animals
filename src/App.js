d41176a3614aae05daf81ea5fd8d6a26ff77a6f7
mij <mijsmatthew@gmail.com>
2023-10-15 17:50:36 -0700

    finish css styling, added .env

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import './App.css'
+
 import { useState } from "react";
 import AnimalShow from "./AnimalShow";
 
@@ -20,9 +22,9 @@ function App() {
     });
 
     return (
-        <div>
+        <div className='app'>
             <button onClick={handleClick}>Add Animal</button>
-            <div>{renderedAnimals}</div>
+            <div className='animal-list'>{renderedAnimals}</div>
         </div>
     )
 }   
